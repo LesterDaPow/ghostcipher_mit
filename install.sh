76d7d6bd104fda79fbd2b914d1c6c937b2fb0c39
Elijah <elijah.sjogren@proton.me>
2025-08-23 19:48:37 -0600

    Fix installation script to clone repository in the correct directory and ensure proper installation path

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ if [ -d "ghostcipher" ]; then
     echo "Updating existing GhostCipher repository..."
     cd ghostcipher
     git pull origin master
+    cd ..  # go back to repo root
 else
     echo "Cloning GhostCipher repository..."
-    git clone https://github.com/LesterDaPow/ghostcipher_mit.git ~/ghostcipher
-    cd ~/ghostcipher
+    git clone https://github.com/LesterDaPow/ghostcipher_mit.git ghostcipher
 fi
 
 echo "Installing GhostCipher..."
-pip install --user -e .
+pip install --user -e ./ghostcipher  # <-- install from repo root
 
 echo "GhostCipher installed/updated successfully!"
